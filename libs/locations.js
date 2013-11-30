e6849ebf5af419209d46049a16b0ae7b6d54dcd5
Reade Lobdill <readelobdill@gmail.com>
2013-11-30 17:28:46 +1100

    revamp. add carousel. begin unipage layout

--- libs/locations.js
+++ libs/locations.js
@@ -2,42 +2,42 @@ var locations = [
 	{
 	  position: new google.maps.LatLng(38.914239, -77.045222),
 	  title: "Glen’s Garden Market, Dupont Circle",
-	  content: "<a class='location-link' target='_blank' href='http://glensgardenmarket.com/'>Glens Garden Market</a><br/><br/><h2>2001 S St NW, Washington, DC </h2>"
+	  content: "<a class='location-link' target='_blank' href='http://glensgardenmarket.com/'>Glens Garden Market</a><br/><br/><h3>2001 S St NW, Washington, DC </h3>"
 	},
 	{
 	  position: new google.maps.LatLng(38.928543, -77.037402),
 	  title: "Each Peach Market, Columbia Heights",
-	  content: "<a class='location-link' target='_blank' href='http://www.eachpeachmarket.com/'>Each Peach Market</a><br/><br/><h2>3068 Mt. Pleasant St NW, Washington, DC</h2>"
+	  content: "<a class='location-link' target='_blank' href='http://www.eachpeachmarket.com/'>Each Peach Market</a><br/><br/><h3>3068 Mt. Pleasant St NW, Washington, DC</h3>"
 	},
 	{
 	  position: new google.maps.LatLng(38.952609, -77.225532),
 	  title: "Nourish Market, Mclean",
-	  content: "<a class='location-link' target='_blank' href='http://www.nourishmarket.com/'>Nourish Market, Mclean</a><br/><br/><h2>8100 Old Dominion Dr, Mclean, VA</h2>"	
+	  content: "<a class='location-link' target='_blank' href='http://www.nourishmarket.com/'>Nourish Market, Mclean</a><br/><br/><h3>8100 Old Dominion Dr, Mclean, VA</h3>"	
 	},
 	{
 	  position: new google.maps.LatLng(38.891377, -77.185371),
 	  title: "Nourish Market, Falls Church",
-	  content: "<a class='location-link' target='_blank' href='http://www.nourishmarket.com/'>Nourish Market, Falls Church</a><br/><br/><h2>1053 W Broad St, Falls Church, VA</h2>"	
+	  content: "<a class='location-link' target='_blank' href='http://www.nourishmarket.com/'>Nourish Market, Falls Church</a><br/><br/><h3>1053 W Broad St, Falls Church, VA</h3>"	
 	},
 	{
 	  position: new google.maps.LatLng(38.933628, -77.175796),
 	  title: "The Organic Butcher of Mclean",
-	  content: "<a class='location-link' target='_blank' href='http://www.theorganicbutcher.com/ml_home.html/'>The Organic Butcher of Mclean</a><br/><br/><h2>6712 Old Dominion Dr  McLean, VA 22101</h2>"	
+	  content: "<a class='location-link' target='_blank' href='http://www.theorganicbutcher.com/ml_home.html/'>The Organic Butcher of Mclean</a><br/><br/><h3>6712 Old Dominion Dr  McLean, VA 22101</h3>"	
 	},
 	{
 	  position: new google.maps.LatLng(38.758980, -77.160940),
 	  title: "Nalls Produce",
-	  content: "<a class='location-link' target='_blank' href='http://www.nallsproduce.com/'>Nalls Produce of Alexandria, VA</a><br/><br/><h2>6712 Old Dominion Dr  McLean, VA 22101</h2>"	
+	  content: "<a class='location-link' target='_blank' href='http://www.nallsproduce.com/'>Nalls Produce of Alexandria, VA</a><br/><br/><h3>6712 Old Dominion Dr  McLean, VA 22101</h3>"	
 	},
 	{
 	  position: new google.maps.LatLng(38.934960, -77.024469),
 	  title: "The Sanctuary",
-	  content: "<a class='location-link' target='_blank' href='https://www.facebook.com/pages/Milk-Cult/131405660359212'>The Sanctuary</a><br/><br/><h2>3632 Georgia Ave NW Washington, DC 20010 </h2>"
+	  content: "<a class='location-link' target='_blank' href='https://www.facebook.com/pages/Milk-Cult/131405660359212'>The Sanctuary</a><br/><br/><h3>3632 Georgia Ave NW Washington, DC 20010 </h3>"
 	},
 	{
 	  position: new google.maps.LatLng(38.955930, -77.069808),
 	  title: "Little Red Fox",
-	  content: "<a class='location-link' target='_blank' href='http://www.littleredfoxdc.com/'>Little Red Fox</a><br/><br/><h2>5035 Connecticut Ave. NW Washington, DC 20008</h2>"
+	  content: "<a class='location-link' target='_blank' href='http://www.littleredfoxdc.com/'>Little Red Fox</a><br/><br/><h3>5035 Connecticut Ave. NW Washington, DC 20008</h3>"
 	}
 ];
 
