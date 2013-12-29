ab86c1fa72002ebb598f254773e4ae499c6ade51
Reade Lobdill <readelobdill@gmail.com>
2013-12-29 13:55:50 +1100

    add logos to bottom of map, fix apple pie story, change top nave layout

--- libs/locations.js
+++ libs/locations.js
@@ -2,42 +2,42 @@ var locations = [
 	{
 	  position: new google.maps.LatLng(38.928543, -77.037402),
 	  title: "Each Peach Market, Columbia Heights",
-	  content: "<a class='location-link' target='_blank' href='http://www.eachpeachmarket.com/'>Each Peach Market</a><br/><br/><h2>3068 Mt. Pleasant St NW, Washington, DC</h2><div class='b-logo each-peach'></div>"
+	  content: "<a class='location-link' target='_blank' href='http://www.eachpeachmarket.com/'>Each Peach Market<h4>3068 Mt. Pleasant St NW, Washington, DC</h4></a>"
 	},
 	{
 	  position: new google.maps.LatLng(38.952609, -77.225532),
 	  title: "Nourish Market, Mclean",
-	  content: "<a class='location-link' target='_blank' href='http://www.nourishmarket.com/'>Nourish Market, Mclean</a><br/><br/><h2>8100 Old Dominion Dr, Mclean, VA</h2><div class='b-logo each-peach'></div>"
+	  content: "<a class='location-link' target='_blank' href='http://www.nourishmarket.com/'>Nourish Market, Mclean<h4>8100 Old Dominion Dr, Mclean, VA</h4></a>"
 	},
 	{
 	  position: new google.maps.LatLng(38.891377, -77.185371),
 	  title: "Nourish Market, Falls Church",
-	  content: "<a class='location-link' target='_blank' href='http://www.nourishmarket.com/'>Nourish Market, Falls Church</a><br/><br/><h2>1053 W Broad St, Falls Church, VA</h2><div class='b-logo nourish-market'></div>"	
+	  content: "<a class='location-link' target='_blank' href='http://www.nourishmarket.com/'>Nourish Market, Falls Church<h4>1053 W Broad St, Falls Church, VA</h4></a>"	
 	},
 	{
 	  position: new google.maps.LatLng(38.933628, -77.175796),
 	  title: "The Organic Butcher of Mclean",
-	  content: "<a class='location-link' target='_blank' href='http://www.theorganicbutcher.com/ml_home.html'>The Organic Butcher of Mclean</a><br/><br/><h2>6712 Old Dominion Dr  McLean, VA 22101</h2><div class='b-logo ob-mclean'></div>"	
+	  content: "<a class='location-link' target='_blank' href='http://www.theorganicbutcher.com/ml_home.html'>The Organic Butcher of Mclean<h4>6712 Old Dominion Dr  McLean, VA 22101</h4></a>"	
 	},
 	{
 	  position: new google.maps.LatLng(38.934960, -77.024469),
 	  title: "The Sanctuary",
-	  content: "<a class='location-link' target='_blank' href='https://www.facebook.com/pages/Milk-Cult/131405660359212'>The Sanctuary</a><br/><br/><h2>3632 Georgia Ave NW Washington, DC 20010 </h2><div class='b-logo milk-cult'></div>"
+	  content: "<a class='location-link' target='_blank' href='https://www.facebook.com/pages/Milk-Cult/131405660359212'>The Sanctuary<h4>3632 Georgia Ave NW Washington, DC 20010 </h4></a>"
 	},
 	{
 	  position: new google.maps.LatLng(38.955930, -77.069808),
 	  title: "Little Red Fox",
-	  content: "<a class='location-link' target='_blank' href='http://www.littleredfoxdc.com/'>Little Red Fox</a><br/><br/><h2>5035 Connecticut Ave. NW Washington, DC 20008</h2><div class='b-logo red-fox'></div>"
+	  content: "<a class='location-link' target='_blank' href='http://www.littleredfoxdc.com/'>Little Red Fox<h4>5035 Connecticut Ave. NW Washington, DC 20008</h4></a>"
 	},
 	{
 	  position: new google.maps.LatLng(38.758980, -77.160940),
 	  title: "Nalls Produce",
-	  content: "<a class='location-link' target='_blank' href='http://www.nallsproduce.com/'>Nalls Produce of Alexandria, VA</a><br/><br/><h2>6712 Old Dominion Dr  McLean, VA 22101</h2><div class='b-logo nalls'></div>"	
+	  content: "<a class='location-link' target='_blank' href='http://www.nallsproduce.com/'>Nalls Produce of Alexandria, VA<h4>6712 Old Dominion Dr  McLean, VA 22101</h4></a>"	
 	},
 	{
 	  position: new google.maps.LatLng(38.658985, -78.225428),
 	  title: "Heritage Hallow Farms",
-	  content: "<a class='location-link' target='_blank' href='http://heritagehollowfarms.net/'>Heritage Hallow at Mount Vernon Farm, VA</a><br/><br/><h2>12018B Lee Highway, Sperryville, Virginia 22740</h2><div class='b-logo hallow'></div>"	
+	  content: "<a class='location-link' target='_blank' href='http://heritagehollowfarms.net/'>Heritage Hallow at Mount Vernon Farm, VA<h4>12018B Lee Highway, Sperryville, Virginia 22740</h4></a>"	
 	}
 ];
 
@@ -66,8 +66,9 @@ function initialize() {
 
 
 		google.maps.event.addListener(marker, "click", function() { 
-			marker.setAnimation(google.maps.Animation.DROP);
-			$('.location-list').html(location.content);
+			infowindow.close();
+			infowindow.setContent(location.content);
+			infowindow.open(map, marker);
 		});
 	});
 }
